043fb9f0dec5fdba5a00d16309e48ce120712895
Luke Donovan <ladbuilder1@gmail.com>
2022-12-19 22:46:55 -0500

    added touch movement support

--- src/app/window/window.component.ts
+++ src/app/window/window.component.ts
@@ -95,11 +95,58 @@ export class WindowComponent implements OnInit {
     document.onmousemove = this.createMouseMove(this);
   }
 
+  moveTouchStart(event: TouchEvent): void {
+    if (!this.draggable) {
+      return;
+    }
+
+    event = event || window.event;
+    event.preventDefault();
+
+    let touch = event.touches.item(0);
+
+    if (touch === null) {
+      console.error('No touching!')
+      return;
+    }
+
+    this.offsetX = this.posX;
+    this.offsetY = this.posY;
+    this.startX = touch.clientX;
+    this.startY = touch.clientY;
+
+    // Update dimensions and unhide outline box
+    let width = this.window.nativeElement.clientWidth;
+    let height = this.window.nativeElement.clientHeight;
+    this.renderer.setStyle(this.outline.nativeElement, 'display', 'block');
+    this.renderer.setStyle(this.outline.nativeElement, 'width', width + 'px')
+    this.renderer.setStyle(this.outline.nativeElement, 'height', height + 'px')
+
+    // Register event handlers
+    document.ontouchend = this.createTouchEnd(this);
+    document.ontouchmove = this.createTouchMove(this);
+  }
+
   createMouseUp(comp: WindowComponent): (event: MouseEvent) => void {
     return function(_event: MouseEvent): void {
       // Deregister event handlers
       document.onmouseup = null;
       document.onmousemove = null;
+
+      // Hide outline box
+      comp.renderer.setStyle(comp.outline.nativeElement, 'display', 'none');
+
+      // Draw window at new position
+      comp.renderer.setStyle(comp.window.nativeElement, 'top', comp.posY + 'px');
+      comp.renderer.setStyle(comp.window.nativeElement, 'left', comp.posX + 'px');
+    }
+  }
+
+  createTouchEnd(comp: WindowComponent): (event: TouchEvent) => void {
+    return function(_event: TouchEvent): void {
+      // Deregister event handlers
+      document.ontouchstart = null;
+      document.ontouchend = null;
 
       // Hide outline box
       comp.renderer.setStyle(comp.outline.nativeElement, 'display', 'none');
@@ -123,8 +170,47 @@ export class WindowComponent implements OnInit {
     }
   }
 
+  createTouchMove(comp: WindowComponent): (event: TouchEvent) => void {
+    return function(event: TouchEvent) {
+      event = event || window.event;
+      event.preventDefault();
+
+      let touch = event.touches.item(0);
+
+      if (touch === null) {
+        console.error('No touching!')
+        return;
+      }
+
+      comp.posX = comp.offsetX + touch.clientX - comp.startX;
+      comp.posY = comp.offsetY + touch.clientY - comp.startY;
+
+      comp.renderer.setStyle(comp.outline.nativeElement, 'top', comp.posY + 'px');
+      comp.renderer.setStyle(comp.outline.nativeElement, 'left', comp.posX + 'px');
+    }
+  }
+
   createResizeMouseUp(comp: WindowComponent): (event: MouseEvent) => void {
     return function(_event: MouseEvent): void {
+      // Deregister event handlers
+      document.onmouseup = null;
+      document.onmousemove = null;
+
+      // Draw window with new dimensions
+      let width = comp.outline.nativeElement.clientWidth;
+      let height = comp.outline.nativeElement.clientHeight;
+      comp.renderer.setStyle(comp.window.nativeElement, 'width', width + 'px')
+      comp.renderer.setStyle(comp.window.nativeElement, 'height', height + 'px')
+      comp.renderer.setStyle(comp.window.nativeElement, 'top', comp.posY + 'px');
+      comp.renderer.setStyle(comp.window.nativeElement, 'left', comp.posX + 'px');
+
+      // Hide outline box
+      comp.renderer.setStyle(comp.outline.nativeElement, 'display', 'none');
+    }
+  }
+
+  createResizeTouchEnd(comp: WindowComponent): (event: TouchEvent) => void {
+    return function(_event: TouchEvent): void {
       // Deregister event handlers
       document.onmouseup = null;
       document.onmousemove = null;
@@ -169,6 +255,40 @@ export class WindowComponent implements OnInit {
     }
   }
 
+  createResizeTouchMove(comp: WindowComponent, direction: string): (event: TouchEvent) => void {
+    return function(event: TouchEvent) {
+      event = event || window.event;
+      event.preventDefault();
+
+      let touch = event.touches.item(0);
+
+      if (touch === null) {
+        console.error('No touching!')
+        return;
+      }
+
+      if (direction.startsWith('bottom')) {
+        comp.newH = comp.startH + touch.clientY - comp.startY;
+        comp.renderer.setStyle(comp.outline.nativeElement, 'height', comp.newH + 'px');
+      } else if (direction.startsWith('top')) {
+        comp.newH = comp.startH - touch.clientY + comp.startY;
+        comp.posY = comp.offsetY + touch.clientY - comp.startY;
+        comp.renderer.setStyle(comp.outline.nativeElement, 'height', comp.newH + 'px');
+        comp.renderer.setStyle(comp.outline.nativeElement, 'top', comp.posY + 'px');
+      }
+
+      if (direction.endsWith('left')) {
+        comp.newW = comp.startW - touch.clientX + comp.startX;
+        comp.posX = comp.offsetX + touch.clientX - comp.startX;
+        comp.renderer.setStyle(comp.outline.nativeElement, 'width', comp.newW + 'px');
+        comp.renderer.setStyle(comp.outline.nativeElement, 'left', comp.posX + 'px');
+      } else if (direction.endsWith('right')) {
+        comp.newW = comp.startW + touch.clientX - comp.startX;
+        comp.renderer.setStyle(comp.outline.nativeElement, 'width', comp.newW + 'px');
+      }
+    }
+  }
+
   resizeMouseDown(event: MouseEvent, direction: string): void {
     if (!this.resizable) {
       return;
@@ -194,6 +314,40 @@ export class WindowComponent implements OnInit {
     // Register event handlers
     document.onmouseup = this.createResizeMouseUp(this);
     document.onmousemove = this.createResizeMouseMove(this, direction);
+  }
+
+  resizeTouchStart(event: TouchEvent, direction: string): void {
+    if (!this.resizable) {
+      return;
+    }
+
+    event = event || window.event;
+    event.preventDefault();
+
+    let touch = event.touches.item(0);
+
+    if (touch === null) {
+      console.error('No touching!')
+      return;
+    }
+
+    this.offsetX = this.posX;
+    this.offsetY = this.posY;
+    this.startX = touch.clientX;
+    this.startY = touch.clientY;
+
+    // Update dimensions and unhide outline box
+    let width = this.window.nativeElement.clientWidth;
+    let height = this.window.nativeElement.clientHeight;
+    this.startW = width;
+    this.startH = height;
+    this.renderer.setStyle(this.outline.nativeElement, 'display', 'block');
+    this.renderer.setStyle(this.outline.nativeElement, 'width', width + 'px')
+    this.renderer.setStyle(this.outline.nativeElement, 'height', height + 'px')
+
+    // Register event handlers
+    document.ontouchend = this.createResizeTouchEnd(this);
+    document.ontouchmove = this.createResizeTouchMove(this, direction);
   }
 }
 
